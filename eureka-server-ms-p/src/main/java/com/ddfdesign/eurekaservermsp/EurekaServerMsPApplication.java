b58004d771e85417a857103c28a9a16272a03100
ddfdesign2018 <43002783+ddfdesign2018@users.noreply.github.com>
2018-09-12 15:10:47 +0200

    Add files via upload

--- eureka-server-ms-p/src/main/java/com/ddfdesign/eurekaservermsp/EurekaServerMsPApplication.java
+++ eureka-server-ms-p/src/main/java/com/ddfdesign/eurekaservermsp/EurekaServerMsPApplication.java
@@ -11,5 +11,6 @@ public class EurekaServerMsPApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(EurekaServerMsPApplication.class, args);
+		System.out.println();
 	}
 }
